882444dcb95baf7681b88e3dc56145ba2fac12da
Anwesh43 <anweshthecool0@gmail.com>
2019-03-21 09:43:30 +0530

    adding drawSBLNode canvas extension method

--- sidebarlineview/src/main/java/com/anwesh/uiprojects/sidebarlineview/SideBarLineView.kt
+++ sidebarlineview/src/main/java/com/anwesh/uiprojects/sidebarlineview/SideBarLineView.kt
@@ -27,3 +27,30 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sjf() : Float = 1f - 2 * (this % 2)
+
+fun Canvas.drawSBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val hGap : Float = (2 * size) / lines
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = foreColor
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc1)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc2.divideScale(j, lines)
+        val x : Float = size * j.sjf() * sc
+        save()
+        translate(-size + hGap * j, 0f)
+        drawLine(hGap / 2, 0f, hGap / 2, x, paint)
+        drawLine(0f, x, hGap, x, paint)
+        restore()
+    }
+    restore()
+}
