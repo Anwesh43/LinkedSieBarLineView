fc85d7d0e72765f1f7a075d517e93df4e1881b99
Anwesh43 <anweshthecool0@gmail.com>
2019-03-21 09:26:21 +0530

    adding libraries and constants

--- sidebarlineview/src/main/java/com/anwesh/uiprojects/sidebarlineview/SideBarLineView.kt
+++ sidebarlineview/src/main/java/com/anwesh/uiprojects/sidebarlineview/SideBarLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.sidebarlineview
 /**
  * Created by anweshmishra on 21/03/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 5
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
