ae12ef19a6228e0fb1c673e8c6dc44f06a319202
Anwesh43 <anweshthecool0@gmail.com>
2019-03-21 09:53:57 +0530

    adding SBLNode class

--- sidebarlineview/src/main/java/com/anwesh/uiprojects/sidebarlineview/SideBarLineView.kt
+++ sidebarlineview/src/main/java/com/anwesh/uiprojects/sidebarlineview/SideBarLineView.kt
@@ -119,4 +119,48 @@ class SideBarLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBLNode(var i : Int, val state : State = State()) {
+
+        private var next : SBLNode? = null
+        private var prev : SBLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SBLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint  : Paint) {
+            canvas.drawSBLNode(i , state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBLNode {
+            var curr : SBLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
